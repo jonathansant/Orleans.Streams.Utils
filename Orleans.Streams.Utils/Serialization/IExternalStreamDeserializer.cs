4a631e9fbb86be539395e6f447684f67a022594e
serpargu <sergioparragjr@gmail.com>
2022-05-09 13:45:44 +0200

    feat: add support for kafka metadata

--- Orleans.Streams.Utils/Serialization/IExternalStreamDeserializer.cs
+++ Orleans.Streams.Utils/Serialization/IExternalStreamDeserializer.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Confluent.Kafka;
+using System;
 
 namespace Orleans.Streams.Utils.Serialization
 {
 	public interface IExternalStreamDeserializer : IDisposable
 	{
 		object Deserialize(QueueProperties queueProps, Type type, byte[] data);
+
+		object Deserialize(QueueProperties queueProps, Type type, ConsumeResult<byte[], byte[]> data)
+			=> Deserialize(queueProps, type, data.Message.Value);
 	}
 }
