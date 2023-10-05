a527b84ee43b0a3a4d492b124baababce279bd90
Jonathan Sant <jonathan.sant@live.co.uk>
2023-10-05 18:40:09 +0200

    feat(*): fix build & revert kafka features which do not belong here (#13)

--- Orleans.Streams.Utils/Serialization/IExternalStreamDeserializer.cs
+++ Orleans.Streams.Utils/Serialization/IExternalStreamDeserializer.cs
@@ -1,13 +1,9 @@
-﻿using Confluent.Kafka;
-using System;
+﻿using System;
 
 namespace Orleans.Streams.Utils.Serialization
 {
 	public interface IExternalStreamDeserializer : IDisposable
 	{
 		object Deserialize(QueueProperties queueProps, Type type, byte[] data);
-
-		object Deserialize(QueueProperties queueProps, Type type, ConsumeResult<byte[], byte[]> data)
-			=> Deserialize(queueProps, type, data.Message.Value);
 	}
 }
